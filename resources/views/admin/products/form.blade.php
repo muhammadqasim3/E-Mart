403a3e6e8d610736faa1dbb5ada75931fdf4361d
Muhammad Qasim <muhammadqasimaslam3@gmail.com>
2021-02-26 21:44:05 +0500

    commit

--- resources/views/admin/products/form.blade.php
+++ resources/views/admin/products/form.blade.php
@@ -14,7 +14,7 @@
 </div>
 <div class="form-group">
    <label>Description</label>
-   <textarea name="description" class="form-control">{{ $product->description }}</textarea>
+   <textarea name="description" class="form-control">{{ $product->description  }}</textarea>
 </div>
 <div class="form-group">
    <label>Price($)</label>
