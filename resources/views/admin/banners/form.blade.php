df75d5a7a0ab218a8d0d02683af3cf3cf20a453a
Muhammad Qasim <muhammadqasimaslam3@gmail.com>
2021-03-28 19:22:56 +0500

    Banner Form

--- resources/views/admin/banners/form.blade.php
+++ resources/views/admin/banners/form.blade.php
@@ -2,6 +2,24 @@
    <label>Name</label>
    <input type="text" class="form-control" placeholder="Enter Banner Name" name="title" value="{{ isset($banner->title) ? $banner->title : null }}" required>
 </div>
+<div class="form-group">
+   <label>Text Syle</label>
+   <input type="text" class="form-control" placeholder="Enter Text Style" name="text_style" value="{{ isset($banner->text_style) ? $banner->text_style : null }}" required>
+</div>
+<div class="form-group">
+   <label>Sort Order</label>
+   <input type="text" class="form-control" placeholder="Enter Sort Order" name="sort_order" value="{{ isset($banner->sort_order) ? $banner->sort_order : null }}" required>
+</div>
+
+<div class="form-group">
+   <label>Content</label>
+   <input type="text" class="form-control" placeholder="Enter Banner Content" name="content" value="{{ isset($banner->content) ? $banner->content : null }}" required>
+</div>
+
+<div class="form-group">
+   <label>Link</label>
+   <input type="text" class="form-control" placeholder="Enter Banner Link" name="link" value="{{ isset($banner->link) ? $banner->link : null }}" required>
+</div>
 
 @if(isset($banner))
 <div class="form-check">
